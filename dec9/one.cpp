7578712f56d3e1befef9418c57b32daf8d8b25f9
Sander in 't Veld <sanderintveld@hotmail.com>
2018-12-09 15:13:47 +0100

    December 9, part one. Takes 100ms.

---
+++ dec9/one.cpp
@@ -0,0 +1,61 @@
+//usr/bin/g++ -g -O2 dec9/one.cpp -o dec9/.one && time ./dec9/.one; exit
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <bitset>
+#include <algorithm>
+#include <cassert>
+
+
+int main(int /*argc*/, char* /*argv*/[])
+{
+	std::ifstream file("dec9/input.txt");
+
+	std::string line;
+	std::getline(file, line);
+
+	int players, maxvalue;
+	sscanf(line.c_str(), "%d players; last marble is worth %d points",
+		&players, &maxvalue);
+
+	std::vector<int> scores(players, 0);
+
+	std::vector<int> marbles = { 0 };
+	marbles.reserve(maxvalue + 1);
+	size_t curpos = 0;
+
+	for (int value = 1; value <= maxvalue; value++)
+	{
+		if (value % 23 == 0)
+		{
+			curpos = (curpos + marbles.size() - 7) % marbles.size();
+
+			// My players are numbered 0 through (player-1), so player 0 placed
+			// marble 0 and player 1 places the marble with value 1.
+			int player = value % players;
+			scores[player] += value;
+			scores[player] += marbles[curpos];
+
+			marbles.erase(marbles.begin() + curpos);
+			if (curpos == marbles.size()) curpos = 0;
+		}
+		else
+		{
+			curpos = ((curpos + 1) % marbles.size()) + 1;
+			marbles.emplace(marbles.begin() + curpos, value);
+		}
+	}
+
+	int maxscore = 0;
+	for (int score : scores)
+	{
+		if (score > maxscore)
+		{
+			maxscore = score;
+		}
+	}
+
+	std::cout << maxscore << std::endl;
+}

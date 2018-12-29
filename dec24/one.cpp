ee65a509063b439c939e4dff944dd778bdab7139
Sander in 't Veld <sanderintveld@hotmail.com>
2018-12-29 17:08:55 +0100

    Finished parsing the input for December 24, part one.

--- dec24/one.cpp
+++ dec24/one.cpp
@@ -44,6 +44,21 @@ uint8_t parseType(std::vector<std::string>& names, const char* buffer)
 	return d;
 }
 
+void parseTypes(std::vector<std::string>& names, const char* buffer,
+	std::bitset<MAXDAMAGETYPECOUNT>& types)
+{
+	std::stringstream strm(buffer);
+	std::string token;
+	while (std::getline(strm, token, ','))
+	{
+		const char* tokenc = token.c_str();
+		if (*tokenc == ' ') tokenc++;
+
+		uint8_t d = parseType(names, tokenc);
+		types[d] = true;
+	}
+}
+
 int main(int /*argc*/, char* /*argv*/[])
 {
 	std::vector<std::string> damagetypenames;
@@ -52,7 +67,7 @@ int main(int /*argc*/, char* /*argv*/[])
 	std::vector<Group> infection;
 
 	{
-		std::ifstream file("dec24/test.txt");
+		std::ifstream file("dec24/input.txt");
 		std::string line;
 		bool parsingImmuneSystem = true;
 		while (std::getline(file, line))
@@ -65,50 +80,49 @@ int main(int /*argc*/, char* /*argv*/[])
 				continue;
 			}
 
-			char immunitiesbuffer[80];
-			char weaknessesbuffer[80];
-			char damagetypebuffer[80];
-
 			Group group;
 			int hits = sscanf(line.c_str(),
-				"%d units each with %d hit points"
-				" (immune to %[^;]; weak to %[^)])"
-				" with an attack that does %d %s damage"
-				" at initiative %d",
-				&(group.units), &(group.hitpoints),
-				immunitiesbuffer, weaknessesbuffer,
-				&(group.damage), damagetypebuffer,
-				&(group.initiative));
-			std::cout << "'" << line << "' ==> " << hits << std::endl;
-			assert(hits == 7);
+				"%d units each with %d hit points",
+				&(group.units), &(group.hitpoints));
+			assert(hits == 2);
 
+			char buffer[80];
+
+			size_t parenpos = line.find('(');
+			if (parenpos != std::string::npos)
 			{
-				std::stringstream strm(immunitiesbuffer);
-				std::string token;
-				while (std::getline(strm, token, ','))
+				if (line[parenpos + 1] == 'i')
 				{
-					const char* tokenc = token.c_str();
-					if (*tokenc == ' ') tokenc++;
+					sscanf(line.c_str() + parenpos + 1,
+						"immune to %[^;)]",
+						buffer);
+					parseTypes(damagetypenames, buffer, group.immunities);
 
-					uint8_t d = parseType(damagetypenames, tokenc);
-					group.immunities[d] = true;
+					parenpos = line.find_first_of(";)", parenpos);
 				}
+
+				if (parenpos != std::string::npos
+					&& line[parenpos + 2] == 'w')
+				{
+					sscanf(line.c_str() + parenpos + 2,
+						"weak to %[^;)]",
+						buffer);
+					parseTypes(damagetypenames, buffer, group.weaknesses);
+				}
+
+				parenpos = line.find(")", parenpos);
+			}
+			else
+			{
+				parenpos = line.find(" with an attack") - 1;
 			}
 
-			{
-				std::stringstream strm(weaknessesbuffer);
-				std::string token;
-				while (std::getline(strm, token, ','))
-				{
-					const char* tokenc = token.c_str();
-					if (*tokenc == ' ') tokenc++;
+			hits = sscanf(line.c_str() + parenpos + 1,
+				" with an attack that does %d %s damage at initiative %d",
+				&(group.damage), buffer, &(group.initiative));
+			assert(hits == 3);
 
-					uint8_t d = parseType(damagetypenames, tokenc);
-					group.weaknesses[d] = true;
-				}
-			}
-
-			group.damagetype = parseType(damagetypenames, damagetypebuffer);
+			group.damagetype = parseType(damagetypenames, buffer);
 		}
 	}
 
